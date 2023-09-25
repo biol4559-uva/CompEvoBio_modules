9e1977fa636ad33810137930bce409e9706384e2
Alan Bergland <aob2x@virginia.edu>
2023-09-25 08:29:53 -0400

    Update runSnakemake.sh

--- utils/snpCalling/runSnakemake.sh
+++ utils/snpCalling/runSnakemake.sh
@@ -16,4 +16,4 @@
 
 module load gcc/9.2.0 openmpi/3.1.6 python/3.7.7 snakemake/6.0.5
 cd /scratch/aob2x/DESTv2/snpCalling
-snakemake --profile /scratch/aob2x/DESTv2/snpCalling/slurm --ri
+snakemake --profile /scratch/aob2x/CompEvoBio_modules/utils/snpCalling/slurm --ri
