a0ca09b5b530d726562d7ee163d5567fd2b713e7
Alan Bergland <aob2x@virginia.edu>
2023-09-26 18:04:40 -0400

    updated data

---
+++ utils/gds_files/combine_metadata.R
@@ -0,0 +1,19 @@
+### libraries
+  library(data.table)
+  library(readxl)
+
+### load meta-data file
+  samps <- fread("/Users/alanbergland/Documents/GitHub/CompEvoBio_modules/utils/gds_files/dest_v2.samps_8Jun2023.csv")
+
+### expevo samples
+  expevo <- as.data.table(read_excel("/Users/alanbergland/Documents/GitHub/CompEvoBio_modules/utils/gds_files/expevo_samps.xlsx"))
+  expevo[long<0 & grepl("North", continent), long:=long*-1]
+  table(expevo$long<0)
+
+### combine
+  samps <- rbind(samps, expevo, fill=T)
+  samps[sampling_strategy=="ExpEvo"]
+  table(samps$set)
+
+### export
+  write.csv(samps, file="/Users/alanbergland/Documents/GitHub/CompEvoBio_modules/utils/gds_files/biol4559_sampleMetadata.csv")
