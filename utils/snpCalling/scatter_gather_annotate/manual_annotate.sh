a6b4dd4cce6df782372100f646f8d1a30e0a8314
Alan Bergland <aob2x@virginia.edu>
2023-10-11 10:28:49 -0400

    Update manual_annotate.sh

--- utils/snpCalling/scatter_gather_annotate/manual_annotate.sh
+++ utils/snpCalling/scatter_gather_annotate/manual_annotate.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #
 #SBATCH -J manual_annotate # A single job name for the array
-#SBATCH --ntasks-per-node=20 # one core
+#SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 2:00:00 ### 1 hours
+#SBATCH -t 6:00:00 ### 1 hours
 #SBATCH --mem 40G
 #SBATCH -o /scratch/aob2x/compBio_SNP_25Sept2023/logs/manual_annotate.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/compBio_SNP_25Sept2023/logs/manual_annotate.%A_%a.err # Standard error
-#SBATCH -p standard
+#SBATCH -p largemem
 #SBATCH --account biol4559-aob2x
 
 ### cat /scratch/aob2x/DESTv2_output_SNAPE/logs/runSnakemake.49369837*.err
 
 ### sbatch /scratch/aob2x/CompEvoBio_modules/utils/snpCalling/scatter_gather_annotate/manual_annotate.sh
-### sacct -j 53998639
+### sacct -j 53998641
 ### cat /scratch/aob2x/DESTv2_output_26April2023/logs/manual_annotate.49572492*.out
 
 module purge
