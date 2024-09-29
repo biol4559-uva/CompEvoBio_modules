2197804bc6b1c6ee230ab55b55dcc592a7887cc1
Alan Bergland <aob2x@virginia.edu>
2024-09-29 09:47:18 -0400

    Update mergeVCF.sh

--- utils/snpCalling/mergeVCF.sh
+++ utils/snpCalling/mergeVCF.sh
@@ -13,8 +13,8 @@
 ### cat /scratch/aob2x/DESTv2_output_SNAPE/logs/runSnakemake.49369837*.err
 
 ### sbatch ~/CompEvoBio_modules/utils/snpCalling/mergeVCF.sh
-### sacct -j 54019600
-### cat /scratch/aob2x/DESTv2_output_26April2023/logs/manual_annotate.49572492*.out
+### sacct -j 64525372
+### cat /scratch/aob2x/compBio_SNP_28Sept2024/manual_annotate.64525372
 
 module load gcc/11.4.0  openmpi/4.1.4 python/3.11.4
 
@@ -30,22 +30,21 @@ mac=50
 version=28Sept2024_ExpEvo
 wd=/scratch/aob2x/compBio_SNP_28Sept2024
 
-bcftools view \
---threads 20 \
-${wd}/dest.${popSet}.${method}.${maf}.${mac}.${version}.norep.vcf.gz | \
-java -jar ~/snpEff/snpEff.jar \
-eff \
-BDGP6.86 - > \
-${wd}/dest.${popSet}.${method}.${maf}.${mac}.${version}.norep.ann.vcf
+# bcftools view \
+# --threads 20 \
+# ${wd}/dest.${popSet}.${method}.${maf}.${mac}.${version}.norep.vcf.gz | \
+# java -jar ~/snpEff/snpEff.jar \
+# eff \
+# BDGP6.86 - > \
+# ${wd}/dest.${popSet}.${method}.${maf}.${mac}.${version}.norep.ann.vcf
+# 
+# 
+# echo "bgzip & tabix"
+#   bgzip -@20 -c ${wd}/dest.${popSet}.${method}.${maf}.${mac}.${version}.norep.ann.vcf > ${wd}/dest.${popSet}.${method}.${maf}.${mac}.${version}.norep.ann.vcf.gz
+#   tabix -f -p vcf ${wd}/dest.${popSet}.${method}.${maf}.${mac}.${version}.norep.ann.vcf.gz
+# 
 
-
-echo "bgzip & tabix"
-  bgzip -@20 -c ${wd}/dest.${popSet}.${method}.${maf}.${mac}.${version}.norep.ann.vcf > ${wd}/dest.${popSet}.${method}.${maf}.${mac}.${version}.norep.ann.vcf.gz
-  tabix -f -p vcf ${wd}/dest.${popSet}.${method}.${maf}.${mac}.${version}.norep.ann.vcf.gz
-
-
-bcftools merge -0 --threads 1 \
+bcftools merge -0 --threads 20 \
 -o /scratch/aob2x/compBio_SNP_25Sept2023/dest.expevo.PoolSeq.PoolSNP.001.50.28Sept2024_ExpEvo.norep.vcf.gz
-/scratch/aob2x/compBio_SNP_25Sept2023/dest.expevo.PoolSNP.001.50.11Oct2023.norep.ann.vcf.gz \
 ${wd}/dest.${popSet}.${method}.${maf}.${mac}.${version}.norep.ann.vcf.gz \
 /project/berglandlab/DEST/dest.all.PoolSNP.001.50.3May2024.ann.vcf.gz
