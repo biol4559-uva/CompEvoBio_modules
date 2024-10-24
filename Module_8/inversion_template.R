ea4313489b16972aef73ad206d6be24e5f7c0bcf
Alan Bergland <aob2x@virginia.edu>
2024-10-24 08:49:12 -0400

    Update inversion_template.R

--- Module_8/inversion_template.R
+++ Module_8/inversion_template.R
@@ -10,78 +10,14 @@
   registerDoMC(20)
 
 ### load this function
-  getData <- function(snps=snp.dt[pos==14617051 & chr=="2L"], samples=samps) {
-    # snps=snp.dt[pos==14617051 & chr=="2L"]; samples=samps[grepl("SRP002024", sampleId)]$sampleId
-
-    ### filter to target
-      seqSetFilter(genofile, variant.id=snps$id, sample.id=samples$sampleId)
-
-    ### get annotations
-      message("Annotations")
-      tmp <- seqGetData(genofile, "annotation/info/ANN")
-      len1 <- tmp$length
-      len2 <- tmp$data
-
-      snp.dt1 <- data.table(len=rep(len1, times=len1),
-                            ann=len2,
-                            id=rep(snps$id, times=len1))
-
-  # Extract data between the 2nd and third | symbol
-    snp.dt1[,class:=tstrsplit(snp.dt1$ann,"\\|")[[2]]]
-    snp.dt1[,gene:=tstrsplit(snp.dt1$ann,"\\|")[[4]]]
-
-  # Collapse additional annotations to original SNP vector length
-    snp.dt1.an <- snp.dt1[,list(n=length(class), col= paste(class, collapse=","), gene=paste(gene, collapse=",")),
-                          list(variant.id=id)]
-
-    snp.dt1.an[,col:=tstrsplit(snp.dt1.an$col,"\\,")[[1]]]
-    snp.dt1.an[,gene:=tstrsplit(snp.dt1.an$gene,"\\,")[[1]]]
-
-  ### get frequencies
-    message("Allele Freqs")
-
-    ad <- seqGetData(genofile, "annotation/format/AD")
-    dp <- seqGetData(genofile, "annotation/format/DP")
-
-    if(class(dp)[1]!="SeqVarDataList") {
-      dp <- list(data=dp)
-    }
-
-
-    af <- data.table(ad=expand.grid(ad$data)[,1],
-                     dp=expand.grid(dp$data)[,1],
-                     sampleId=rep(seqGetData(genofile, "sample.id"), dim(ad$data)[2]),
-                     variant.id=rep(seqGetData(genofile, "variant.id"), each=dim(ad$data)[1]))
-
-  ### tack them together
-    message("merge")
-    afi <- merge(af, snp.dt1.an, by="variant.id")
-    afi <- merge(afi, snps, by.x="variant.id", by.y="id")
-
-    afi[,af:=ad/dp]
-
-  ### calculate effective read-depth
-    afis <- merge(afi, samples[,c("sampleId", "set", "nFlies", "locality",
-                                  "lat", "long", "continent", "country", "province", "city",
-                                  "min_day", "max_day", "min_month", "max_month", "year", "jday",
-                                  "bio_rep", "tech_rep", "exp_rep", "loc_rep", "subsample", "sampling_strategy",
-                                  "SRA_Accession"), with=F], by="sampleId")
-
-    afis[chr=="X|Y", nEff:=round((dp*nFlies - 1)/(dp+nFlies))]
-    afis[chr!="X", nEff:=round((dp*2*nFlies - 1)/(dp+2*nFlies))]
-    afis[,af_nEff:=round(af*nEff)/nEff]
-    setnames(afis, "col", "annotation")
-  ### return
-    afis[,-c("n"), with=F]
-  }
+  source("https://raw.githubusercontent.com/biol4559-uva/CompEvoBio_modules/refs/heads/main/utils/misc/getData_function.R")
 
 ### open GDS file
-  genofile <- seqOpen("/scratch/aob2x/compBio_SNP_25Sept2023/dest.expevo.PoolSNP.001.50.11Oct2023.norep.ann.gds")
+  genofile <- seqOpen("/scratch/aob2x/dest.expevo.PoolSeq.PoolSNP.001.50.28Sept2024_ExpEvo.norep.gds")
   genofile
 
 ### load meta-data file
-  samps <- fread("/scratch/aob2x/GDS/biol4559_sampleMetadata.csv")
-  samps[is.na(set), set:="ExpEvo"]
+  samps <- fread("https://raw.githubusercontent.com/biol4559-uva/CompEvoBio_modules/refs/heads/main/data/full_sample_metadata.28Sept2024_ExpEvo.csv")
 
 ### load in inversion markers
   inv.markers <- fread("https://raw.githubusercontent.com/biol4559-uva/CompEvoBio_modules/main/Module_8/inversion_markers_v6.txt")
