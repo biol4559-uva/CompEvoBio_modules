005d60b37484a1a5457104271a22a86b7fd71072
Alan Bergland <aob2x@virginia.edu>
2025-09-29 21:43:58 -0400

    Update manual_annotate.sh

--- utils/snpCalling/scatter_gather_annotate/manual_annotate.sh
+++ utils/snpCalling/scatter_gather_annotate/manual_annotate.sh
@@ -3,16 +3,16 @@
 #SBATCH -J manual_annotate # A single job name for the array
 #SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 6:00:00 ### 1 hours
+#SBATCH -t 12:00:00 ### 1 hours
 #SBATCH --mem 40G
 #SBATCH -o /scratch/aob2x/29Sept2025_ExpEvo/logs/manual_annotate.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/29Sept2025_ExpEvo/logs/manual_annotate.%A_%a.err # Standard error
-#SBATCH -p largemem
-#SBATCH --account biol4559-aob2x
+#SBATCH -p standard
+#SBATCH --account berglandlab
 
 ### cat /scratch/aob2x/DESTv2_output_SNAPE/logs/runSnakemake.49369837*.err
 
-### sbatch /scratch/aob2x/CompEvoBio_modules/utils/snpCalling/scatter_gather_annotate/manual_annotate.sh
+### sbatch ~/CompEvoBio_modules/utils/snpCalling/scatter_gather_annotate/manual_annotate.sh
 ### sacct -j 53998649
 ### cat /scratch/aob2x/DESTv2_output_26April2023/logs/manual_annotate.49572492*.out
 # # ijob -A biol4559-aob2x -c10 -p largemem --mem=40G
