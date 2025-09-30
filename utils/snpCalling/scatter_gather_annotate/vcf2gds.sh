bfa3ceb1cb50c97b587dfb8db9fba33baf95b0da
Alan Bergland <aob2x@virginia.edu>
2025-09-30 08:59:36 -0400

    Update vcf2gds.sh

--- utils/snpCalling/scatter_gather_annotate/vcf2gds.sh
+++ utils/snpCalling/scatter_gather_annotate/vcf2gds.sh
@@ -3,7 +3,7 @@
 #SBATCH -J vcf2gds # A single job name for the array
 #SBATCH --ntasks-per-node=10 # one core
 #SBATCH -N 1 # on one node
-#SBATCH -t 0:02:00  ### 48 hours
+#SBATCH -t 0:04:00  ### 48 hours
 #SBATCH --mem 24G
 #SBATCH -o /scratch/aob2x/dest/slurmOutput/vcf2gds.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/dest/slurmOutput/vcf2gds.%A_%a.err # Standard error
@@ -14,7 +14,7 @@
 ### sacct -j 50241880
 ### cat /scratch/aob2x/dest/slurmOutput/vcf2gds.22867938
 
-module load gcc/7.1.0 openmpi/3.1.4 R/3.6.3
+module load htslib/1.17 bcftools/1.17 parallel/20200322 gcc/11.4.0 openmpi/4.1.4 R/4.3.1 samtools vcftools
 
-Rscript --vanilla /scratch/aob2x/CompEvoBio_modules/utils/snpCalling/scatter_gather_annotate/vcf2gds.R \
-/project/berglandlab/DEST/vcf/dest.all.PoolSNP.001.50.8Jun2023.norep.AT_EScorrect.ann.vcf.gz
+Rscript --vanilla ~/CompEvoBio_modules/utils/snpCalling/scatter_gather_annotate/vcf2gds.R \
+/scratch/aob2x/compBio_SNP_29Sept2025/dest.PoolSeq.PoolSNP.001.50.29Sept2025_ExpEvo.norep.ann.vcf.gz
