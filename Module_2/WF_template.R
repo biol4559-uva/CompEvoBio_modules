7501a33df5feccbc65cdf325d4282a634533c6aa
Alan Bergland <aob2x@virginia.edu>
2026-09-03 08:58:09 -0400

    small chagnes

--- Module_2/WF_template.R
+++ Module_2/WF_template.R
@@ -1,5 +1,4 @@
 ### libraries
-  .libPaths(c("~/USERNAME", .libPaths())) ### <- you'll need to change this to your username
   library(ggplot2)
   library(data.table)
   library(foreach)  ### you will probably need to install this package
@@ -41,6 +40,7 @@
   tmp[gen==1]$af <- startingAlleleFreq ### we need to initialize the very first generation at the specified allele frequency
 
 ### we run the for loop. We start at generation two because we need to use generation 1 as the starting generation
+### this style of "for" loop can look in the "past" (i-1) and, if we wanted, the "future" (i+1)
   for(i in 2:nGens) {
     tmp[gen==i]$af <- rbinom(1, popSize, tmp[gen==(i-1)]$af)/popSize
   }
@@ -49,11 +49,14 @@
   ggplot(data=tmp, aes(x=gen, y=af)) + geom_line() + ylim(0,1)
 
 
-## Multiple alleles
+## loops that do not care about the past and cannot see it. But, it is useful for independent simulations
   test_fun <- function(popSize) {
     return(data.table(initialPopSize=popSize, newPopSize=popSize * 2))
   }
 
-  tmp <- foreach(popSize.i=c(100, 500), .combine="rbind")%do%{
+  tmp2 <- foreach(popSize.i=c(100, 500), .combine="rbind")%do%{
     test_fun(popSize=popSize.i)
   }
+
+### Your turn: combine the "for" and "foreach" loops, and likely a function that you write, 
+### to simulate different population sizes and a few hundred loci per population size for several hundred generations
