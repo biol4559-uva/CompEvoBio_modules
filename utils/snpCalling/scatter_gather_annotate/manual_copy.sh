dfced4501365a404d68226c549e8f13cc36feefb
Alan Bergland <aob2x@virginia.edu>
2026-09-22 07:53:32 -0400

    copy

--- utils/snpCalling/scatter_gather_annotate/manual_copy.sh
+++ utils/snpCalling/scatter_gather_annotate/manual_copy.sh
@@ -8,12 +8,12 @@
 #SBATCH -o /scratch/aob2x/29Sept2025_ExpEvo/logs/manual_gather.%A_%a.out # Standard output
 #SBATCH -e /scratch/aob2x/29Sept2025_ExpEvo/logs/manual_gather.%A_%a.err # Standard error
 #SBATCH -p standard
-#SBATCH --account berglandlab_standard
+#SBATCH --account biol4020-aob2x
 
 
 # ijob -A berglandlab -c10 -p standard --mem=50G
-# sbatch --array=1-177 ~/CompEvoBio_modules/utils/snpCalling/scatter_gather_annotate/manual_copy.sh
-# sacct -j 4287319
+# sbatch --array=1-105 ~/CompEvoBio_modules/utils/snpCalling/scatter_gather_annotate/manual_copy.sh
+# sacct -j 20363782
 # cat /scratch/aob2x/29Sept2025_ExpEvo/logs/manual_gather.4242823_1.out
 
 # SLURM_ARRAY_TASK_ID=1
@@ -26,5 +26,5 @@ if [[ ! -d "/project/berglandlab/DEST/dest_mapped/ExpEvo/$samp" ]]; then
 fi
 
 ls /standard/BerglandTeach/mapping_output/${samp}/*.masked.sync.gz*
-#cp /standard/BerglandTeach/mapping_output/${samp}/*.masked.sync.gz* /project/berglandlab/DEST/dest_mapped/ExpEvo/$samp
+cp /standard/BerglandTeach/mapping_output/${samp}/*.masked.sync.gz* /project/berglandlab/DEST/dest_mapped/ExpEvo/$samp
 touch /project/berglandlab/DEST/dest_mapped/ExpEvo/$samp/*.masked.sync.gz.tbi
